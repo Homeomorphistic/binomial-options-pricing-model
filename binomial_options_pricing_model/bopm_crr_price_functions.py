d8282913515f2bbe7cf22a878a1c1d22ca5001cd
Homeomorphistic <chmiela.bartosz@gmail.com>
2024-05-05 11:50:47 +0200

    Cleaned up and documented plotting.

--- binomial_options_pricing_model/bopm_crr_price_functions.py
+++ binomial_options_pricing_model/bopm_crr_price_functions.py
@@ -1,19 +1,25 @@
 """One-dimensional slices of the price function.
 
+Price of an option (in CRR model) is a multivariable function: V(r, S_0, K, delta_t, T, sigma).
+First step in analyzing price of an option is to look at one-dimensional slices of price function.
+
+This script contains functions used later for plotting:
+V(r)$, V(S_0), V(K), V(delta_t), V(T), V(sigma).
 They are vectorized (a loop), for easy plotting later.
-Default values of constant parameters are taken from the sliders.
-This way, each plot will change after change in one parameter.
+
+IMPORTANT: the purpose of this script is only for testing static plots
+(without interactive sliders)
 """
-import bopm
+import binomial_options_pricing_model.bopm as bopm
 
 import numpy as np
-from typing import Callable
+from typing import Callable, Tuple
 
 
 def vectorize(func: Callable, arg: np.ndarray) -> np.ndarray:
     """Vectorize a function which takes only one scalar argument.
 
-    Loop over arg and obtain return for each iteraton. Return it
+    Loop over arg and obtain return for each iteration. Return it
     as a vector.
 
     :param func: a function which takes only one scalar argument.
@@ -27,7 +33,18 @@ def vectorize(func: Callable, arg: np.ndarray) -> np.ndarray:
     return V
 
 
-def v_one_dim_slices(r, S, K, delta_t, T, sigma):
+def crr_price_functions(r: float, S: float, K: float,
+                        delta_t: float, T: float, sigma: float) -> Tuple[Callable]:
+    """Return price functions in CRR model.
+
+    :param r: default risk-free rate of the market.
+    :param S: default current underlying asset price.
+    :param K: default strike price.
+    :param delta_t: default time step (in years).
+    :param T: default maturity of an option (in years).
+    :param sigma: default volatility of an asset.
+    :returns: tuple of price functions of each parameter.
+    """
     def v_r(r: np.ndarray, american=False, call=True) -> np.ndarray:
         func = lambda arg: bopm.crr_price_option(arg, S, K, delta_t, T,sigma, american, call)[0]
         return vectorize(func, r)
@@ -44,7 +61,6 @@ def v_one_dim_slices(r, S, K, delta_t, T, sigma):
         func = lambda arg: bopm.crr_price_option(r, S, K, arg, T, sigma, american, call)[0]
         return vectorize(func, delta_t)
 
-
     def v_t(T: np.ndarray, american=False, call=True) -> np.ndarray:
         func = lambda arg: bopm.crr_price_option(r, S, K, delta_t, arg, sigma, american, call)[0]
         return vectorize(func, T)
@@ -53,4 +69,4 @@ def v_one_dim_slices(r, S, K, delta_t, T, sigma):
         func = lambda arg: bopm.crr_price_option(r, S, K, delta_t, T, arg, american, call)[0]
         return vectorize(func, sigma)
 
-    return [v_r, v_s, v_k, v_delta_t, v_t, v_sigma]
+    return v_r, v_s, v_k, v_delta_t, v_t, v_sigma
--- binomial_options_pricing_model/plot_crr.py
+++ binomial_options_pricing_model/plot_crr.py
@@ -8,23 +8,36 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def plot_crr_one_dim_slices(r, S, K, delta_t, T, sigma, domain, v_functions):
+def plot_crr_price_functions(r, S, K, delta_t, T, sigma, domain, price_functions):
+    """Plot price functions of each parameter in CRR model.
+
+    :param r: default risk-free rate of the market.
+    :param S: default current underlying asset price.
+    :param K: default strike price.
+    :param delta_t: default time step (in years).
+    :param T: default maturity of an option (in years).
+    :param sigma: default volatility of an asset.
+    :param price_functions: tuple of price functions of each parameter.
+    """
     figure, axis = plt.subplots(2, 3)
 
-    params = [r, S, K, delta_t, T, sigma]
+    params = r, S, K, delta_t, T, sigma
     x_labels = ["risk-free rate r", "spot price $S_0$", "strike price K",
                 "time step $\Delta t$", "maturity T", "volatility $\sigma$"]
 
     for i in range(len(params)):
         param = params[i]
-        v_func = v_functions[i]
+        v_func = price_functions[i]
+        # Domain of the parameter.
         param = np.linspace(domain[0] * param, domain[1] * param)
 
+        # Compute price of vanilla options. (american call == european call)
         V_eu_call = v_func(param, american=False, call=True)
         V_eu_put = v_func(param, american=False, call=False)
-        # V_am_call = v_func(param, american=True, call=True) AM CALL = EU CALL
+        # V_am_call = v_func(param, american=True, call=True)
         V_am_put = v_func(param, american=True, call=False)
 
+        # Plot on 2 x 3 grid.
         axis[i % 2, i % 3].plot(param, V_eu_call, color="blue")
         axis[i % 2, i % 3].plot(param, V_eu_put, color="red")
         # axis[i%2, i%3].plot(param, V_am_call, color="blue")
@@ -38,9 +51,10 @@ def plot_crr_one_dim_slices(r, S, K, delta_t, T, sigma, domain, v_functions):
 
 
 if __name__ == "__main__":
-    from bopm_one_dim_slices import v_one_dim_slices
-    r, S, K, delta_t, T, sigma = .02, 50, 40, 1/12, 2, .3
-    domain = (0.8, 1.2)
-    v_functions = v_one_dim_slices(r, S, K, delta_t, T, sigma)
+    from bopm_crr_price_functions import crr_price_functions
 
-    plot_crr_one_dim_slices(r, S, K, delta_t, T, sigma, domain, v_functions)
+    r, S, K, delta_t, T, sigma = .02, 50, 48, 1 / 12, 2, .3
+    domain = (0.8, 1.2)
+    price_functions = crr_price_functions(r, S, K, delta_t, T, sigma)
+
+    plot_crr_price_functions(r, S, K, delta_t, T, sigma, domain, price_functions)
